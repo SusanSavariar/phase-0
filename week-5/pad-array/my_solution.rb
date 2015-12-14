3873e7e0e4a5bea273ef5ba5ee21d06ee0b7f83f
SusanSavariar <SusanSavariar@users.noreply.github.com>
2015-12-14 14:24:43 -0500

    Update my_solution.rb

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -36,5 +36,17 @@ end
 # 3. Refactored Solution
 
 
-
 # 4. Reflection
+Were you successful in breaking the problem down into small steps?
+-I would say yes.
+Once you had written your pseudocode, were you able to easily translate it into code?  What difficulties and successes did you have?
+-Yes, the steps were simple and so was the code to write.
+Was your initial solution successful at passing the tests?  If so, why do you think that is?  If not, what were the errors you encountered and what did you do to resolve them?
+-No it was not successful. We didn't so the pseudocode properly and didn't break down the steps like we should have. 
+When you refactored, did you find any existing methods in Ruby to clean up your code?
+-No the code seemed pretty short. 
+How readable is your solution?  Did you and your pair choose descriptive variable names?
+-I think it is pretty readable.  Yes we did.
+What is the difference between destructive and non-destructive methods in your own words.
+-Destructive methods change the original information passed to it, while non-destructive methods return altered information without changing the original.
+=end
