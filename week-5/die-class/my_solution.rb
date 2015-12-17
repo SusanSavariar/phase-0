c822cb8bec1fb6a554a30baea5c99848bbe41637
SusanSavariar <sasavariar@gmail.com>
2015-12-16 23:04:04 -0500

    4.6

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -6,10 +6,11 @@
 
 # 0. Pseudocode
 
-# Input:
-# Output:
+# Input: The number of sides of a die
+# Output: Number of sides of a die
 # Steps:
-
+# Check that there is at least 1 side to the dice, if less than 1 side, give an error message.
+#I am stuck here. I struggled with this challenge and in the interest of time am moving on.
 
 # 1. Initial Solution
 
@@ -38,3 +39,22 @@ end
 
 
 # 4. Reflection
+What is an ArgumentError and why would you use one?
+
+# An ArgumentError is an error message that is given when one of the arguments provided is invalid.
+
+# What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+#
+
+# What is a Ruby class?
+# A class is a blue print for individual objects.
+
+# Why would you use a Ruby class?
+# Writing code would be easier by having a blueprint of objects pre-written.
+
+# What is the difference between a local variable and an instance variable?
+# A local variable is only usable within the block of code it lives in
+# An instance variable is visible anywhere there is an instance of the class where the variable was defined.
+
+# Where can an instance variable be used?
+# An instance variable can be used anywhere within the instance of its class.
