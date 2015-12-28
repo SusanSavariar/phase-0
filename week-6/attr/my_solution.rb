eab372984fcc57023f7e4d4e024650bca9700409
SusanSavariar <sasavariar@gmail.com>
2015-12-27 23:29:02 -0500

    add files

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,26 @@
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+Status API Training Shop Blog About Pricing
+© 2015 GitHub, Inc. Terms Privacy Security Contact Help
